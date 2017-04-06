dd44a7b293b7d85e3521cf599cf3591a680b3f2b
Pavel Cvrček <jasnapaka@jasnapaka.com>
2017-04-06 16:26:57 +0200

    Odstranění nadbytečného kódu

--- import.php
+++ import.php
@@ -19,12 +19,6 @@ if (!ini_set('default_socket_timeout', 900)) {
 	die ("Nepodarilo se nastavit casovy limit pro sitove spojeni.");
 }
 
-// Kontrola dostupnosti dat
-/*$json_data = @file_get_contents(DATA);
-if ($json_data === FALSE) {
-	die("Nepodarilo se nacist data o parkovani kol (JSON).");
-}*/
-
 $writer = new ParkingWriter(new ParkingDb());
 $result = $writer->process();
 
